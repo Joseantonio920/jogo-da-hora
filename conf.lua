dc033dca6a4d950ee48daa73d3422a86c3724472
Joseantonio920 <71590697+Joseantonio920@users.noreply.github.com>
2021-04-11 11:42:57 -0300

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,35 @@
+function love.conf(t)
+	t.console=false
+	t.window.title="jogo da hora"
+	t.window.icon=nil
+	t.window.width=512
+	t.window.height=256
+	t.window.minwidth=512
+	t.window.minheight=256
+	t.window.borderless=true
+	t.window.resizable=false
+	t.window.fullscreen=false
+	t.window.fullscreentype="desktop"
+	t.window.vsync=true
+	t.window.display=nil
+	t.window.x=nil
+	t.window.y=nil
+	
+	t.modules.audio=true
+	t.modules.sound=true
+	t.modules.physics=false
+	t.modules.mouse=false
+	t.modules.math=true
+	t.modules.keyboard=true
+	t.modules.joystick=false
+	t.modules.image=true
+	t.modules.data=true
+	t.modules.event=true
+	t.modules.font=true
+	t.modules.graphics=true
+	t.modules.window=true
+	t.modules.video=false
+	t.modules.touch=true
+	t.modules.thread=true
+	t.modules.system=true
+end
---
+++ main.lua
@@ -0,0 +1,6 @@
+function love.draw()
+	love.graphics.print("ola mundo", 0, 0)
+end
+function love.update(dt)
+	
+end
